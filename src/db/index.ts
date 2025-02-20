6c9b92f4442abb91d0ded8c57c09ae4b472f5cd0
Haziq Hairil <haziqhairil21@gmail.com>
2025-02-21 03:20:37 +0800

    Import DATABASE_URL env var from Astro

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,4 +1,5 @@
+import { DATABASE_URL } from "astro:env/server";
 import { drizzle } from "drizzle-orm/node-postgres";
 
-export const db = drizzle(import.meta.env.DATABASE_URL);
+export const db = drizzle(DATABASE_URL);
 export * from "./schema";
