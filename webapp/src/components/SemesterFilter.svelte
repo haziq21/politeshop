180740325bd80d2d632781e4cafb6462e0e843ab
Haziq Hairil <haziqhairil21@gmail.com>
2025-02-26 01:12:23 +0800

    Keyboard-focusability for semester filter

--- webapp/src/components/SemesterFilter.svelte
+++ webapp/src/components/SemesterFilter.svelte
@@ -18,9 +18,9 @@
   {#each options as sem, i}
     <label
       class="bg-stone-800 hover:bg-stone-700 has-checked:bg-stone-600 text-xs text-stone-400
-      has-checked:text-stone-200 px-3 py-2 rounded-md cursor-pointer"
+      has-checked:text-stone-200 focus-within:ring px-3 py-2 rounded-md cursor-pointer"
     >
-      <input type="radio" name="filter" value={sem.id} class="hidden" bind:group={selectedId} />
+      <input type="radio" name="filter" value={sem.id} class="absolute opacity-0" bind:group={selectedId} />
       {sem.name}
     </label>
   {/each}
