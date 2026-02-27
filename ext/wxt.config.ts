504e39d8c78a066d1fa314dcbfa8341a4d5ed7ee
Haziq Hairil <haziqhairil21@gmail.com>
2026-02-28 01:02:25 +0800

    Fix POLITEMall image loading bug

--- ext/wxt.config.ts
+++ ext/wxt.config.ts
@@ -4,7 +4,20 @@ import { defineConfig } from "wxt";
 export default defineConfig({
   manifest: {
     name: "POLITEShop Extension",
-    permissions: ["cookies", "declarativeNetRequest", "declarativeNetRequestWithHostAccess"],
-    host_permissions: ["https://*.polite.edu.sg/"],
+    permissions: [
+      "cookies",
+      "declarativeNetRequest",
+      "declarativeNetRequestWithHostAccess",
+    ],
+    host_permissions: ["https://*.polite.edu.sg/", "http://*.localhost/*"],
+    declarative_net_request: {
+      rule_resources: [
+        {
+          id: "fix_orb",
+          enabled: true,
+          path: "rules/fix_orb.json",
+        },
+      ],
+    },
   },
 });
